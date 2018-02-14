86c69b6c98b13f530c00ae62b7f54e437f3ce391
kennethteh90 <kennethteh90@gmail.com>
2018-02-14 12:04:42 +0800

    added functions to call API and handleClick

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,72 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+<div id="app">
+
+  <div>
+    <h1>Welcome to our feedback system</h1>
+    <h2>Please tell us how your meal went so we can do even better next time!</h2>
   </div>
+
+  <div>
+    <table>
+      <thead>
+        <th>Order ID</th>
+        <th>Feedback</th>
+      </thead>
+
+      <tbody>
+        <tr v-for="order in orders">
+
+          <td> {{ order.order_id }} </td>
+          <td v-if="order.feedback_submitted"> Thanks for sharing your feedback! </td>
+          <td v-if="!order.feedback_submitted"> <button v-on:click="handleClick(order.order_id)">Submit Feedback</button> </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+      orders: { },
+      selectedOrder: ""
+    }
+  },
+
+  methods: {
+
+    fetchOrders() {
+      fetch("https://food-delivery-api.herokuapp.com/orders")
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        return response
+      })
+      .then(results => results.json())
+      .then(res => {
+        console.log(res.orders)
+        this.orders = res.orders
+      })
+    },
+
+    handleClick: function(order_id) {
+      this.selectedOrder = order_id
+      console.log("clicked")
+      console.log(this.selectedOrder)
+    },
+
+  },
+
+  mounted() {
+    // call this.apiCall here
+    this.fetchOrders()
+  },
+
 }
 </script>
 
@@ -25,4 +79,36 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+table {
+  margin: 2em;
+  padding: 2em;
+}
+
+th {
+  font-size: 1.2em;
+  border-bottom: 1px solid black;
+  padding: .5em 2em;
+}
+
+td {
+  padding: .5em 2em;
+}
+
+button {
+  border-radius: 0;
+  border: none;
+  padding: .5em 3em;
+  margin: .5em 1em;
+  color: #fff;
+  background-color: #007bff;
+  border-color: #007bff;
+}
+
+button:hover {
+  color: #fff;
+  background-color: #0069d9;
+  border-color: #0062cc;
+}
+
 </style>
