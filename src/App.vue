852d94043c64a790eff6eaf4e3b3ca66278de626
kennethteh90 <kennethteh90@gmail.com>
2018-02-14 12:21:25 +0800

    handle if no orders returned

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,117 @@
 <template>
-<div id="app">
+  <div id="app">
 
-  <div>
-    <h1>Welcome to our feedback system</h1>
-    <h2>Please tell us how your meal went so we can do even better next time!</h2>
+    <div>
+      <h1>Welcome to our feedback system</h1>
+      <h2>Please tell us how your meal went so we can do even better next time!</h2>
+      <hr>
+    </div>
+
+    <div>
+      <h3 v-if="Object.keys(orders).length === 0">Nothing here yet! Head on over to our ordering page to get some awesome food!</h3>
+      <table v-if="orders.length">
+        <thead>
+          <th>Order ID</th>
+          <th>Feedback</th>
+        </thead>
+
+        <tbody>
+          <tr v-for="order in orders">
+
+            <td> {{ order.order_id }} </td>
+            <td v-if="order.feedback_submitted"> Thanks for sharing your feedback! </td>
+            <td v-if="!order.feedback_submitted"> <button v-on:click="handleClick(order.order_id)">Submit Feedback</button> </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
   </div>
-
-  <div>
-    <table>
-      <thead>
-        <th>Order ID</th>
-        <th>Feedback</th>
-      </thead>
-
-      <tbody>
-        <tr v-for="order in orders">
-
-          <td> {{ order.order_id }} </td>
-          <td v-if="order.feedback_submitted"> Thanks for sharing your feedback! </td>
-          <td v-if="!order.feedback_submitted"> <button v-on:click="handleClick(order.order_id)">Submit Feedback</button> </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-
-</div>
 </template>
 
 <script>
-export default {
+  export default {
 
-  data() {
-    return {
-      orders: { },
-      selectedOrder: ""
-    }
-  },
-
-  methods: {
-
-    fetchOrders() {
-      fetch("https://food-delivery-api.herokuapp.com/orders")
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText)
-        }
-        return response
-      })
-      .then(results => results.json())
-      .then(res => {
-        console.log(res.orders)
-        this.orders = res.orders
-      })
+    data() {
+      return {
+        orders: { },
+        selectedOrder: ""
+      }
     },
 
-    handleClick: function(order_id) {
-      this.selectedOrder = order_id
-      console.log("clicked")
-      console.log(this.selectedOrder)
+    methods: {
+
+      fetchOrders() {
+        fetch("https://food-delivery-api.herokuapp.com/orders")
+        .then(function(response) {
+          if (!response.ok) {
+            throw Error(response.statusText)
+          }
+          return response
+        })
+        .then(results => results.json())
+        .then(res => {
+          console.log(res.orders)
+          this.orders = res.orders
+        })
+      },
+
+      handleClick: function(order_id) {
+        this.selectedOrder = order_id
+        console.log("clicked")
+        console.log(this.selectedOrder)
+      },
+
     },
 
-  },
+    mounted() {
+      // call this.apiCall here
+      this.fetchOrders()
+    },
 
-  mounted() {
-    // call this.apiCall here
-    this.fetchOrders()
-  },
-
-}
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-table {
-  margin: 2em;
-  padding: 2em;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
-th {
-  font-size: 1.2em;
-  border-bottom: 1px solid black;
-  padding: .5em 2em;
-}
+  table {
+    margin: 2em;
+    padding: 2em;
+  }
 
-td {
-  padding: .5em 2em;
-}
+  th {
+    font-size: 1.2em;
+    border-bottom: 1px solid black;
+    padding: .5em 2em;
+  }
 
-button {
-  border-radius: 0;
-  border: none;
-  padding: .5em 3em;
-  margin: .5em 1em;
-  color: #fff;
-  background-color: #007bff;
-  border-color: #007bff;
-}
+  td {
+    padding: .5em 2em;
+  }
 
-button:hover {
-  color: #fff;
-  background-color: #0069d9;
-  border-color: #0062cc;
-}
+  button {
+    border-radius: 0;
+    border: none;
+    padding: .5em 3em;
+    margin: .5em 1em;
+    color: #fff;
+    background-color: #007bff;
+    border-color: #007bff;
+  }
+
+  button:hover {
+    color: #fff;
+    background-color: #0069d9;
+    border-color: #0062cc;
+  }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+/* eslint-disable */
 new Vue({
   el: '#app',
   components: { App },
